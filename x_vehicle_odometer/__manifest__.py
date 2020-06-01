794b8b57e39e637658fecbe247e3cb113b717976
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2020-06-01 14:50:36 +0700

    [action] x_vehicle_odometer : change inline edit to form edit

---
+++ x_vehicle_odometer/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Custom - Vehicle Odometer',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'category': 'Fleet, Vehicle',
+    'description': """
+Custom Vehicle Odometer :
+-------------
+    * Added a description field to Odometer log
+        """,
+    'depends': ['fleet'],
+    'summary': 'Custom - Vehicle Odometer',
+    'data': [
+        'actions/x_vehicle_odometer_act_window.xml',
+    ],
+    'installable': True,
+}
