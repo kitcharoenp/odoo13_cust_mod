2d8209add3dd5a00e9a74e5476804c5b07e048da
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2020-08-17 07:35:09 +0000

    [models] x_fleet_fuel_log : enable auto-create odometer when create fuel log

--- x_fleet_fuel_log/models/x_fleet_vehicle_cost.py
+++ x_fleet_fuel_log/models/x_fleet_vehicle_cost.py
@@ -18,18 +18,3 @@ class FleetVehicleCost(models.Model):
         ('cash', 'Cash')],
         string='Payment Type',
         help='Payment Type (Cash or Fleet Card)')
-
-    # Override: _set_odometer()
-    def _set_odometer(self):
-        for record in self:
-            if not record.odometer:
-                raise UserError(_('Emptying the odometer value of a vehicle is not allowed.'))
-            # disable create odometer log
-            '''
-            odometer = self.env['fleet.vehicle.odometer'].create({
-                'value': record.odometer,
-                'date': record.date or fields.Date.context_today(record),
-                'vehicle_id': record.vehicle_id.id
-            })
-            '''
-            self.odometer_id = odometer
