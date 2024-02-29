c6448825a8b00de2a3072d79561d9db97d67e820
Kitcharoen Poolperm <kitcharoenp@gmail.com>
2024-02-29 09:25:03 +0000

    [models] x_fleet_fuel_log : Enable tracking the odometer value by `mail.activity.mixin`

--- x_fleet_fuel_log/models/x_fleet_fuel_log.py
+++ x_fleet_fuel_log/models/x_fleet_fuel_log.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
-
+from odoo.exceptions import UserError
 
 class FleetVehicleLogFuel(models.Model):
-    _inherit = 'fleet.vehicle.log.fuel'
+    _name  = 'fleet.vehicle.log.fuel'
+    _inherit = ['fleet.vehicle.log.fuel', 'mail.thread', 'mail.activity.mixin']
 
     x_fuel_type = fields.Selection([
         ('gasoline', 'Gasoline'),
@@ -18,6 +19,33 @@ class FleetVehicleLogFuel(models.Model):
         ('cash', 'Cash')],
         string='Payment Type',
         help='Payment Type (Cash or Fleet Card)')
+    
+    # Overide `odometer` field
+    odometer = fields.Float(
+        compute="_get_odometer"
+        , inverse='_set_odometer'
+        , string='Odometer Value'
+        , tracking=True
+        , help='Odometer measure of the vehicle at the moment of this log')
+
+
+    def _get_odometer(self):
+        self.odometer = 0.0
+        for record in self:
+            record.odometer = False
+            if record.odometer_id:
+                record.odometer = record.odometer_id.value
+
+    def _set_odometer(self):
+        for record in self:
+            if not record.odometer:
+                raise UserError(_('Emptying the odometer value of a vehicle is not allowed.'))
+            odometer = self.env['fleet.vehicle.odometer'].create({
+                'value': record.odometer,
+                'date': record.date or fields.Date.context_today(record),
+                'vehicle_id': record.vehicle_id.id
+            })
+            self.odometer_id = odometer
 
     # Overide original method
     @api.onchange('liter', 'price_per_liter', 'amount')
